34681b72e81855e8ac17b1f87875c9f7102e737b
genesis castellano <alexaguerra184@gmail.com>
2024-06-05 09:03:31 -0700

    Add files via upload

---
+++ Entidad/ECategoria.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+
+
+namespace Entidad
+{
+   
+    public class User
+    {
+        public int Id { get; set; }
+        public string usuario { get; set; }
+        public string contraseña { get; set; }
+        public string Contraseña { get; set; }
+        public string Nombre { get; set; }
+        public string Telefono { get; set; }
+    }
+
+   
+    
+
+
+}
